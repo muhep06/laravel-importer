4883264c88152b6a868527e3420991aa0d9dab1d
Selennurdlsz <selennurdilsiz67@gmail.com>
2019-08-20 13:20:38 +0300

    Update Importer.php

--- package/acikveri/importer/src/Interfaces/Importer.php
+++ package/acikveri/importer/src/Interfaces/Importer.php
@@ -6,5 +6,14 @@ namespace AcikVeri\Importer\Interfaces;
 
 interface Importer
 {
+    public function loadFromUrl($url);
+    public function loadFromString($data);
+    public function setIndex(string $key);
+    public function setTable($table);
+    public function insert($column, $key);
+    public function getIncludes();
+    public function getIndex();
+    public function update();
+    public function import();
 
 }
