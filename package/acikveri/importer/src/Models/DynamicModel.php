835dcece735f07c3f939e23a637b8eab2d194b0f
Muhep Atasoy <muhepatasoy06@gmail.com>
2019-08-20 10:48:23 +0300

    Create DynamicModel.php

---
+++ package/acikveri/importer/src/Models/DynamicModel.php
@@ -0,0 +1,29 @@
+<?php
+
+
+namespace AcikVeri\Importers\Models;
+
+
+use Webpatser\Uuid\Uuid;
+use Illuminate\Database\Eloquent\Model;
+
+class DynamicModel extends Model
+{
+    use BindsDynamically;
+
+    static $useUuid = false;
+
+    public static function boot()
+    {
+        parent::boot();
+            self::creating(function ($model) {
+                if (self::$useUuid) {
+                    $model->uuid = (string) Uuid::generate(4);
+                }
+            });
+    }
+
+    public function setUseUuid(bool $use) {
+        self::$useUuid = $use;
+    }
+}
