ccf63682561db03d113f90cfb57406cb91218fea
Muhep Atasoy <muhepatasoy06@gmail.com>
2019-08-20 10:48:13 +0300

    Create XMLImporterRelation.php

---
+++ package/acikveri/importer/src/XMLImporter/XMLImporterRelation.php
@@ -0,0 +1,75 @@
+<?php
+
+
+namespace Muhep\ExchangeRates\Utils;
+
+
+use AcikVeri\Importer\XMLImporter\XMLImporter;
+use AcikVeri\Importers\Models\DynamicModel;
+use SimpleXMLElement;
+
+class XMLImporterRelation
+{
+    private $model;
+    private $parser;
+    private $xml;
+    private $loopIndex;
+
+    public function setModel(DynamicModel $model): void
+    {
+        $this->model = $model;
+    }
+
+
+    public function setParser(XMLImporter $parser): void
+    {
+        $this->parser = $parser;
+    }
+
+
+    public function setXml(SimpleXMLElement $xml): void
+    {
+        $this->xml = $xml;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getModel(): DynamicModel
+    {
+        return $this->model;
+    }
+
+    /**
+     * @return XMLImporter
+     */
+    public function getParser()
+    {
+        return $this->parser;
+    }
+
+    /**
+     * @return SimpleXMLElement
+     */
+    public function getXml()
+    {
+        return $this->xml;
+    }
+
+    /**
+     * @param integer $loopIndex
+     * @return void
+     */
+    public function setLoopIndex($loopIndex)
+    {
+        $this->loopIndex = $loopIndex;
+    }
+
+    /**
+     * @return int
+     */
+    public function getLoopIndex()
+    {
+        return $this->loopIndex;
+    }
+}
