f7bf2281f1aeef2590087ca4c2c9a583bfce0fb4
Muhep Atasoy <muhepatasoy06@gmail.com>
2019-08-20 13:38:15 +0300

    Create 2019_08_19_162940_create_exchange_rates_table.php

---
+++ database/migrations/2019_08_19_162940_create_exchange_rates_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateExchangeRatesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('exchange_rates', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('unit');
+            $table->string('isim');
+            $table->string('forexBuying')->nullable();
+            $table->string('forexSelling')->nullable();
+            $table->string('banknoteBuying')->nullable();
+            $table->string('banknoteSelling')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('exchange_rates');
+    }
+}
